f107358a1c42064eefa299bd93ce79e47792bf48
Chris Diaz <77657338+cd-vulcan@users.noreply.github.com>
2021-10-01 01:37:22 -0400

    new
    2nd attempt

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eveonlinetool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("i fixed it!");
+        }
+    }
+}
