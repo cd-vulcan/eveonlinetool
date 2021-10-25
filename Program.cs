74617e96f123ac39ff7d186bf157a33537e1307c
Chris Diaz <77657338+cd-vulcan@users.noreply.github.com>
2021-10-25 16:24:09 -0400

    last class
    updates and api

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace eveonlinetool
 {
@@ -7,6 +10,8 @@ namespace eveonlinetool
         static void Main(string[] args)
         {
             Console.WriteLine("i fixed it!");
+            var client = new HttpClient();
+            var types=client.GetStringAsync("https://market.fuzzwork.co.uk/aggregates/?region=10000002&types=34,35,36,37,38,39,40").Result;
         }
     }
 }
